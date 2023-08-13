83a66e5d676104b49722f283ab067e909de478f1
jolega <alonso_juancho@hotmail.com>
2023-08-13 10:32:14 -0500

    separate routes

--- models/server.js
+++ models/server.js
@@ -7,6 +7,7 @@ class Server {
     constructor() {
         this.app  = express ()
         this.port = process.env.PORT
+        this.userPath = '/api/users'
 
         // Middleware
 
@@ -15,7 +16,6 @@ class Server {
         //routes
         this.routes();
     }
-
 
 
 
@@ -28,36 +28,7 @@ class Server {
 
     routes(){
 
-        this.app.get('/api', (req, res) => {
-            res.json({
-                msg: 'get API'
-           })
-          })
-
-        this.app.put('/api', (req, res) => {
-            res.json({
-                msg: 'put API'
-           })
-          })
-
-        this.app.post('/api', (req, res) => {
-            res.status(201).json({
-                msg: 'post API'
-           })
-          })
-
-        
-        this.app.delete('/api', (req, res) => {
-            res.json({
-                msg: 'delete API'
-           })
-          })
-
-          this.app.patch('/api', (req, res) => {
-            res.json({
-                msg: 'patch API'
-           })
-          })
+        this.app.use(this.userPath, require('../routes/user'))
 
     }
 
